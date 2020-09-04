de0942aaa9bbb85513228988dfa2bfca9e68420f
Mauricio Coutinho <mauricio.coutinho04@hotmail.com>
2020-09-04 09:44:04 -0300

    ProjetoIntegrador

---
+++ impactmais/src/test/java/org/zeroseis/impactmais/ImpactmaisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zeroseis.impactmais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpactmaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
